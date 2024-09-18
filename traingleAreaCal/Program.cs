e6cb04982be352ffb28c54e746ac22264e276c2e
Dianara Kristy D. Garciano <142156759+mosshead19@users.noreply.github.com>
2024-09-18 12:20:14 +0800

    created leap year

--- traingleAreaCal/Program.cs
+++ traingleAreaCal/Program.cs
@@ -8,8 +8,30 @@ namespace traingleAreaCal
 {
     internal class Program
     {
+        // Area = √s(s - a)(s - b)(s - c)
+        //where s is half the perimeter, or(a + b + c)/2
+
+        public static double triangleArea(double a, double b, double c)
+        {
+            double s = (a + b + c)/2;
+
+            double x = s*(s-a)*(s-b)*(s-c);
+
+            double Area = Math.Sqrt(x);
+
+            return Area;
+            
+        }
         static void Main(string[] args)
         {
+            //test case
+            double a = 5.0;
+            double b = 6.0;
+            double c = 7.0;
+
+            double area = triangleArea(a, b, c);
+            Console.WriteLine(area);
+            //14.6969384566991
         }
     }
 }
